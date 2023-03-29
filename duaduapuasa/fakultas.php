2a8e4a3cb95996911fe41d0b450160f6db426c68
adelptri <129275384+adelptri@users.noreply.github.com>
2023-03-29 17:03:56 +0700

    Add files via upload

---
+++ duaduapuasa/fakultas.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Selamat sore pejuang es cendol<br>";
+
+echo "<br>";
+
+$slogan="Gateway"; 
+echo"<br>";
+$bilangan_pertama=204;
+
+$pernyataan=false;
+
+echo"slogan saya adalah $slogan, alamat saya di nomor $bilangan_pertama<br>";
+echo"saya adalah fans Arsenal, $pernyataan<br>";
+
+if ($pernyataan == false){
+    echo "alhamdulilah berkah<br>";
+}else if($pernyataan == true){
+    echo "aduh";
+}
+?>
---
+++ duaduapuasa/proses.php
@@ -0,0 +1,7 @@
+<?php
+$nilai_1=$_POST["x"];
+$nilai_2=$_POST["y"];
+
+$hasil = $nilai_1 + $nilai_2;
+echo "$nilai_1 + $nilai_2 = $hasil";
+?>
