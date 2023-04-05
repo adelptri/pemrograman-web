9da1cf480664ac3bbca34baffebdb36965a46588
adelptri <129275384+adelptri@users.noreply.github.com>
2023-04-05 17:13:26 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+	<header>
+		<title>menghitung nilai akhir</title>
+	</header> 
+	<body>
+		<h2>form input</h2>
+		<form method="post" action="proses.php">
+        Nilai kuis : <input type="text" name="nilai_kuis" /><br /> <br />
+        Nilai UTS : <input type="text" name="nilai_uts" /><br /><br />
+        Nilai UAS : <input type="text" name="nilai_uas" /><br /><br />
+            <input type="submit" value="Submit">
+        </form>
+	</body>
+</html>
